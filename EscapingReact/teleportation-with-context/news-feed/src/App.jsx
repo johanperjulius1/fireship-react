8078452589de65702b450076e738b80b9504fba9
johanperjulius1 <johan.per.julius.berglund@gmail.com>
2026-07-30 13:58:02 +0200

    starting on newsfeed challenge

---
+++ EscapingReact/teleportation-with-context/news-feed/src/App.jsx
@@ -0,0 +1,77 @@
+import * as React from "react";
+
+const videoPlaybackContext = React.createContext({
+  playingVideoId: null,
+  setPlayingVideoId: () => {}
+});
+
+function VideoPlaybackProvider({ children }) {
+  const playingVideoId = null;
+  const setPlayingVideoId = () => {};
+
+  return null;
+}
+
+function VideoItem({ videoId, title, poster, src }) {
+  const videoIsActive = false;
+
+  const handleTogglePlay = () => {};
+
+  return (
+    <li>
+      <h3>{title}</h3>
+      <article>
+        <video poster={poster}>
+          <source src={src} type="video/mp4" />
+        </video>
+        <button
+          title={videoIsActive ? "Pause" : "Play"}
+          onClick={handleTogglePlay}
+        >
+          {videoIsActive ? "⏸" : "▶"}
+        </button>
+      </article>
+    </li>
+  );
+}
+
+function NewsFeed() {
+  const videos = [
+    {
+      id: 1,
+      title: "The React Way",
+      poster: "https://react.gg/img/visualized-og2.jpg",
+      src:
+        "https://stream.mux.com/TbVCJiOghmISJgg4AznPfFHYRfiVoek8OJHF56Y01oR4/high.mp4"
+    },
+    {
+      id: 2,
+      title: "The History of the Web",
+      poster: "https://react.gg/img/visualized-og1.jpg",
+      src:
+        "https://stream.mux.com/EwJPlEBa0046jGSVdYOnRsX9WnqHjytgIBXwkOt7LvVg/high.mp4"
+    },
+    {
+      id: 3,
+      title: "Rendering, Visualized",
+      poster: "https://react.gg/img/visualized-og5.jpg",
+      src:
+        "https://stream.mux.com/VvQKMwPEOq5BUnc9eRN4sL5sUEZrHqWxNlCbpXSkE3I/high.mp4"
+    }
+  ];
+
+  return (
+    <div>
+      <h1>News Feed</h1>
+      <ul></ul>
+    </div>
+  );
+}
+
+export default function App() {
+  return (
+    <VideoPlaybackProvider>
+      <NewsFeed />
+    </VideoPlaybackProvider>
+  );
+}
