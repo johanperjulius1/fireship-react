43ad1b45b7f5d97b295be3e51373553370915d1b
johanperjulius1 <johan.per.julius.berglund@gmail.com>
2026-07-31 16:55:05 +0200

    another version of reducer practice

---
+++ state-practice-2/src/App.jsx
@@ -0,0 +1,35 @@
+import { useReducer } from "react"
+
+function reducer (state, value){
+  const nextState = state + value
+
+  console.log(
+    `Reducer invoked. state: ${state}, value: ${value}, nextState: ${nextState}`
+  )
+  
+  return nextState
+}
+
+const initialState = 0
+
+function App() {
+  const [state, dispatch] = useReducer(reducer, initialState)
+
+  const handleIncrement = () => {
+    dispatch(1)
+  }
+
+  return (
+    <>
+      {state}
+      {/* <button onClick = {() => dispatch(1)}>+</button> */}
+
+      <button onClick = {handleIncrement}>+</button>
+
+      {/* <button>-</button> */}
+    </>
+  )
+}
+
+
+export default App
