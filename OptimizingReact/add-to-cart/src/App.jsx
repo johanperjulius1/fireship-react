ec39207b0e8a9d1a22aee5e19f219ff4f6bba555
johanperjulius1 <johan.per.julius.berglund@gmail.com>
2026-08-18 19:56:40 +0200

    completing the add-to-cart challenge.

---
+++ OptimizingReact/add-to-cart/src/App.jsx
@@ -0,0 +1,117 @@
+import * as React from "react";
+
+const products = [
+  { id: 1, name: "Poké Ball", price: 10 },
+  { id: 2, name: "Great Ball", price: 20 },
+  { id: 3, name: "Ultra Ball", price: 30 }
+];
+
+function calculateTotal(cart) {
+  return cart.reduce((total, item) => total + item.price * item.quantity, 0);
+}
+
+const initialState = [];
+
+function reducer(cart, action) {
+  switch (action.type) {
+    case "add": {
+      const existingItem = cart.find((item) => item.id === action.id);
+
+      if (existingItem) {
+        return cart.map((item) =>
+          item.id === action.id
+            ? { ...item, quantity: item.quantity + 1 }
+            : item
+        );
+      }
+
+      const product = products.find((product) => product.id === action.id);
+      return [...cart, { ...product, quantity: 1 }];
+    }
+
+    case "update": {
+      return cart
+        .map((item) => {
+          if (item.id !== action.id) return item;
+
+          const nextQuantity =
+            action.adjustment === "increment"
+              ? item.quantity + 1
+              : item.quantity - 1;
+
+          return { ...item, quantity: nextQuantity };
+        })
+        .filter((item) => item.quantity > 0);
+    }
+
+    default:
+      return cart;
+  }
+}
+
+export default function ShoppingCart() {
+  const [cart, dispatch] = React.useReducer(reducer, initialState);
+
+  const handleAddToCart = (id) => dispatch({ type: "add", id });
+
+  const handleUpdateQuantity = (id, adjustment) => {
+    dispatch({
+      type: "update",
+      id,
+      adjustment
+    });
+  };
+
+  return (
+    <main>
+      <h1>Poké Mart</h1>
+      <section>
+        <div>
+          <ul className="products">
+            {products.map((product) => (
+              <li key={product.id}>
+                {product.name} - ${product.price}
+                <button
+                  className="primary"
+                  onClick={() => handleAddToCart(product.id)}
+                >
+                  Add to cart
+                </button>
+              </li>
+            ))}
+          </ul>
+        </div>
+      </section>
+      <hr />
+      <aside>
+        <div>
+          <h2>Shopping Cart</h2>
+          <ul>
+            {cart.map((item) => (
+              <li key={item.id}>
+                {item.name}
+                <div>
+                  <button
+                    onClick={() => handleUpdateQuantity(item.id, "decrement")}
+                  >
+                    -
+                  </button>
+                  {item.quantity}
+                  <button
+                    onClick={() => handleUpdateQuantity(item.id, "increment")}
+                  >
+                    +
+                  </button>
+                </div>
+              </li>
+            ))}
+            {!cart.length && <li>Cart is empty</li>}
+          </ul>
+        </div>
+        <hr />
+
+        <h3>Total: ${calculateTotal(cart)}</h3>
+      </aside>
+    </main>
+  );
+}
