cd5636cd0671efdeea55a878c4ee9f08116fe832
johanperjulius1 <johan.per.julius.berglund@gmail.com>
2026-08-19 18:35:27 +0200

    reducer practice

--- state-practice-3/src/App.jsx
+++ state-practice-3/src/App.jsx
@@ -1,11 +1,49 @@
+// import { useReducer } from "react";
+
+// function Counter() {
+//   const [state, dispatch] = useReducer(reducer, 0)
+
+//   function reducer (state, action){
+//     return state + action
+//   }
+
+
+//   return (
+//     <>
+//       {state}
+//       <button onClick={() => dispatch(1)}>Add</button>
+//       <button onClick={() => dispatch(-1)}>Decrease</button>
+//     </>
+//   );
+// }
+
+// export default Counter
+
+import { useReducer } from "react";
+
 function Counter() {
-  let count = 0;
-  console.log("button is rerendered")
+  const [state, dispatch] = useReducer(reducer, 0)
+
+  function reducer(state, action) {
+    switch (action.type) {
+      case "increase":
+        return state + 1
+        break;
+      case "decrease":
+        return state - 1
+        break;
+      default:
+        return state;
+    }
+  }
+
 
   return (
-    <button onClick={() => count++}>
-      {count}
-    </button>
+    <>
+      {state}
+      <button onClick={() => dispatch({type: "increase"})}>Add</button>
+      <button onClick={() => dispatch({type: "decrease"})}>Decrease</button>
+    </>
   );
 }
 
