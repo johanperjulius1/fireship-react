cce523f0be154586524464303f3a565f2aefb22f
johanperjulius1 <johan.per.julius.berglund@gmail.com>
2026-08-19 14:11:38 +0200

    Option A implementation

--- state-practice-5/src/App.jsx
+++ state-practice-5/src/App.jsx
@@ -1,4 +1,31 @@
 import { useState, useEffect, useRef } from "react"
 
+function App() {
+  const [state, setState] = useState(0)
+  const [isPlaying, setIsPlaying] = useState(false)
+
+  const handleToggle = () => {
+    setIsPlaying(isPlaying => !isPlaying)
+  }
+
+  useEffect(() => {
+    if(isPlaying === true){
+      const id = setInterval(() => {
+        setState(state => state + 1)
+      }, 1000)
+      
+          return () => {
+            clearInterval(id)
+          }
+    }
+  }, [isPlaying])
+
+  return (
+    <div>
+      {state}
+      <button onClick={handleToggle}>{isPlaying ? "Pause" : "Play"}</button>
+    </div>
+  )
+}
 
 export default App;
